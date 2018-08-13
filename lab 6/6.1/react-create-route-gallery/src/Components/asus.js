3e0c166263fb1d7a2962015b6d83968fb072077f
Himanshu Sagar <hsagarthegr8@gmail.com>
2018-08-13 10:24:12 +0530

    lab 6 done

--- lab 6/6.1/react-create-route-gallery/src/Components/asus.js
+++ lab 6/6.1/react-create-route-gallery/src/Components/asus.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 
+import asus1 from '../img/asus1.jpg'
+import asus2 from '../img/asus2.jpg'
+import asus3 from '../img/asus3.jpg'
+import asus4 from '../img/asus4.jpg'
+
 class Asus extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            imgs: [ asus1, asus2, asus3, asus4 ]
+         }
     }
     render() { 
+        let images = this.state.imgs.map((image,index) => {
+             return <img className="img-thumbnail" width="580px" src={image} alt={index+1} key={index}/>
+        })
         return ( 
-            <h1>Asus</h1>
+            <div>
+            <h3>Asus</h3>
+            {images}
+            </div>
          );
     }
 }
--- lab 6/6.1/react-create-route-gallery/src/Components/oneplus.js
+++ lab 6/6.1/react-create-route-gallery/src/Components/oneplus.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import oneplus1 from '../img/oneplus1.jpg'
+import oneplus2 from '../img/oneplus2.jpg'
+import oneplus3 from '../img/oneplus3.jpg'
+import oneplus4 from '../img/oneplus4.jpg'
 
 class OnePlus extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            imgs: [ oneplus1, oneplus2, oneplus3, oneplus4 ]
+         }
     }
     render() { 
+        let images = this.state.imgs.map((image,index) => {
+             return <img className="img-thumbnail" width="580px" src={image} alt={index+1} key={index}/>
+        })
         return ( 
-            <h1>OnePlus</h1>
+            <div>
+            <h3>OnePlus</h3>
+            {images}
+            </div>
          );
     }
 }
